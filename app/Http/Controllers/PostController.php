38f54f4b1a64c883bfbc4197d2bf62364cba04ae
Muhammad Rafli <raflylagitobat09@gmail.com>
2023-01-05 00:47:43 +0700

    init

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -12,7 +12,7 @@ class PostController extends Controller
         
     return view('blog', [
         "title" => "Blog",
-        "posts"  => Post::all()
+        "posts"  => Post::all() //diambil dari sini yang di web
     ]);
     }
 
